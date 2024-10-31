08707b04fd030d1c452766d43decc4ca9e033929
jeff-0324 <jeffap324@gmail.com>
2024-10-31 16:30:58 +0900

    [feat] : Lv3 구현

--- 1week_personal/1week_personal project.playground/Contents.swift
+++ 1week_personal/1week_personal project.playground/Contents.swift
@@ -1,49 +1,71 @@
 import Foundation
 
-/*
- 1. Lv1
- - [ ]  더하기, 빼기, 나누기, 곱하기 연산을 수행할 수 있는 Calculator 클래스를 만들기
- - [ ]  생성한 클래스를 이용하여 연산을 진행하고 출력
-*/
- 
-/*
- 
- 2. Lv2
- - [ ]  Lv1에서 만든 Calculator 클래스에 “나머지 연산”이 가능하도록 코드를 추가하고, 연산 진행 후 출력
- */
-
 // claulator 함수를 정의
-class calculator {
+class Calculator {
+    
+    // 입력 받을 값의 변수를 선언
+    let value1: Double
+    let value2: Double
+    let operatorValue: String
+    
+    // init을 통해 값을 입력받아서 사용
+    init(value1: Double, value2: Double, operatorValue: String) {
+        self.value1 = value1
+        self.value2 = value2
+        self.operatorValue = operatorValue
+    }
+    
+    // 연산자에 맞는 함수 호출
+    @MainActor func distingishOperator() -> Double {
+     
+        var result: Double = 0              // 계산된 결과값을 대입해 반환하기 위해
+        
+        switch operatorValue {
+        case "+" :
+            result = AddOperation().calculate(a: value1, b: value2)
+        case "-" :
+            result = SubtractOperation().calculate(a: value1, b: value2)
+        case "*" :
+            result = MultiplyOperation().calculate(a: value1, b: value2)
+        case "/" :
+            result = DivideOperation().calculate(a: value1, b: value2)
+        default : fatalError
+            
+        }
+        return result
+    }
+    
+}
 
-    // 각각의 연산 메서드 정의
-    func add (a : Double, b: Double) -> Double {
+// 연산자별 클래스를 생성해 단일 책임의 원칙에 부합하게 생성
+class AddOperation {
+    func calculate(a: Double, b: Double) -> Double {
         return a + b
     }
-    func substract (a : Double, b: Double) -> Double {
+}
+class SubtractOperation {
+    func calculate(a: Double, b: Double) -> Double {
         return a - b
-    }
-    func multiply (a : Double, b: Double) -> Double {
-        return a * b
-    }
-    func divide (a : Double, b: Double) -> Double {
-        return a / b
-    }
-    // Lv2. 나머지 연산이 가능한 함수 추가
-    func modulo (a: Double, b: Double) -> Double {
-        return a.truncatingRemainder(dividingBy: b)     // double타입은 '%'을 사용할 수 없어 'truncatingRemainder(dividingBy:)' 사용
     }
 }
 
-// 입력 값 생성
-let inputValue1: Double = 42
-let inputValue2: Double = 12
+class MultiplyOperation {
+    func calculate(a: Double, b: Double) -> Double {
+        return a * b
+    }
+}
 
-// 인스턴스 생성 후 변수에 할당
-let calculate = calculator()
-print(calculate.add(a: inputValue1, b: inputValue2))
-print(calculate.substract(a: inputValue1, b: inputValue2))
-print(calculate.multiply(a: inputValue1, b: inputValue2))
-print(calculate.divide(a: inputValue1, b: inputValue2))
-print(calculate.modulo(a: inputValue1, b: inputValue2))
+class DivideOperation {
+    func calculate(a: Double, b: Double) -> Double {
+        guard a != 0 && b != 0 else {
+            return 0
+        }
+        return a / b
+    }
+}
 
+// 인스턴스 생성 후 값을 입력
+let calculator = Calculator(value1: 10, value2: 20, operatorValue: "*")
+print(calculator.distingishOperator())
 
+let a = A()  
---
+++ 1week_personal/1week_personal project.playground/Sources/Calculator.swift
@@ -0,0 +1,12 @@
+//
+//  Calculator.swift
+//  
+//
+//  Created by jae hoon lee on 10/31/24.
+//
+
+public class A  {
+    public init() {
+        
+    }
+}
