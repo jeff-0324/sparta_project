d333d833630e7cdb17ca6157d9d2ce9b39d90ef7
jeff-0324 <jeffap324@gmail.com>
2024-10-31 17:23:44 +0900

    [feat] : protocol를 사용해 추상화 적용

--- 1week_personal/1week_personal project.playground/Contents.swift
+++ 1week_personal/1week_personal project.playground/Contents.swift
@@ -1,71 +1,7 @@
 import Foundation
 
-// claulator 함수를 정의
-class Calculator {
-    
-    // 입력 받을 값의 변수를 선언
-    let value1: Double
-    let value2: Double
-    let operatorValue: String
-    
-    // init을 통해 값을 입력받아서 사용
-    init(value1: Double, value2: Double, operatorValue: String) {
-        self.value1 = value1
-        self.value2 = value2
-        self.operatorValue = operatorValue
-    }
-    
-    // 연산자에 맞는 함수 호출
-    @MainActor func distingishOperator() -> Double {
-     
-        var result: Double = 0              // 계산된 결과값을 대입해 반환하기 위해
-        
-        switch operatorValue {
-        case "+" :
-            result = AddOperation().calculate(a: value1, b: value2)
-        case "-" :
-            result = SubtractOperation().calculate(a: value1, b: value2)
-        case "*" :
-            result = MultiplyOperation().calculate(a: value1, b: value2)
-        case "/" :
-            result = DivideOperation().calculate(a: value1, b: value2)
-        default : fatalError
-            
-        }
-        return result
-    }
-    
-}
-
-// 연산자별 클래스를 생성해 단일 책임의 원칙에 부합하게 생성
-class AddOperation {
-    func calculate(a: Double, b: Double) -> Double {
-        return a + b
-    }
-}
-class SubtractOperation {
-    func calculate(a: Double, b: Double) -> Double {
-        return a - b
-    }
-}
-
-class MultiplyOperation {
-    func calculate(a: Double, b: Double) -> Double {
-        return a * b
-    }
-}
-
-class DivideOperation {
-    func calculate(a: Double, b: Double) -> Double {
-        guard a != 0 && b != 0 else {
-            return 0
-        }
-        return a / b
-    }
-}
 
 // 인스턴스 생성 후 값을 입력
-let calculator = Calculator(value1: 10, value2: 20, operatorValue: "*")
+let calculator = Calculator(value1: 45, value2: 53, operatorValue: "*")
 print(calculator.distingishOperator())
 
-let a = A()  
--- 1week_personal/1week_personal project.playground/Sources/Calculator.swift
+++ 1week_personal/1week_personal project.playground/Sources/Calculator.swift
@@ -1,12 +1,71 @@
-//
-//  Calculator.swift
-//  
-//
-//  Created by jae hoon lee on 10/31/24.
-//
+import Foundation
 
-public class A  {
-    public init() {
-        
+// 프로토콜을 선언해서 공통된 작업을 수행하도록 구성
+public protocol AbstractOperation {
+    func calculate (a: Double, b: Double) -> Double
+}
+
+// claulator 함수를 정의
+// 다른 파일(디렉토리)에서 접근할 수 있도록 public을 사용해 접근 가능하게 변경
+public class Calculator {
+    
+    // 입력 받을 값의 변수를 선언
+    let value1: Double
+    let value2: Double
+    let operatorValue: String
+    
+    // init을 통해 값을 입력받아서 사용
+    public init(value1: Double, value2: Double, operatorValue: String) {
+        self.value1 = value1
+        self.value2 = value2
+        self.operatorValue = operatorValue
+    }
+    
+    // 연산자에 맞는 함수 호출
+    public func distingishOperator() -> Double {
+    
+        // 입력 받은 연산자에 맞는 클래스내의 함수를 호출해 값을 반환한다.
+        switch operatorValue {
+        case "+" :
+            return  AddOperation().calculate(a: value1, b: value2)
+        case "-" :
+            return  SubtractOperation().calculate(a: value1, b: value2)
+        case "*" :
+            return  MultiplyOperation().calculate(a: value1, b: value2)
+        case "/" :
+            return  DivideOperation().calculate(a: value1, b: value2)
+        default :
+            print("잘못된 연산자입니다.")
+            return 0
+            
+        }
+    }
+}
+
+// 연산자별 클래스를 생성해 단일 책임의 원칙에 부합하게 생성
+// 공통된 코드를 추상화한 프로토콜을 채택해서 사용
+class AddOperation: AbstractOperation {
+    func calculate(a: Double, b: Double) -> Double {
+        return a + b
+    }
+}
+class SubtractOperation: AbstractOperation {
+    func calculate(a: Double, b: Double) -> Double {
+        return a - b
+    }
+}
+
+class MultiplyOperation: AbstractOperation {
+    func calculate(a: Double, b: Double) -> Double {
+        return a * b
+    }
+}
+
+class DivideOperation: AbstractOperation {
+    func calculate(a: Double, b: Double) -> Double {
+        guard a != 0 && b != 0 else {
+            return 0
+        }
+        return a / b
     }
 }
